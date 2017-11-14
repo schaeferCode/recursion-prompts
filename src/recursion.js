53eaa15dff427ef4abe958feaeec1cb06ab73db6
Scott Schaefer <scottschaef@gmail.com>
2017-11-14 13:07:50 -0500

    Power of 2 and reverse string recursively

--- src/recursion.js
+++ src/recursion.js
@@ -116,10 +116,24 @@ var exponent = function(base, exp) {
 // powerOfTwo(16); // true
 // powerOfTwo(10); // false
 var powerOfTwo = function(n) {
+  if (n === 1) {
+    return true;
+  }
+  if (n % 1 !== 0 || n === 0) {
+    return false;
+  }
+  return powerOfTwo(n/2);
 };
 
 // 9. Write a function that reverses a string.
 var reverse = function(string) {
+  if (string.length === 0) {
+    return [];
+  }
+  var lastIndex = string.length - 1;
+  var result = [];
+  result.push(string[lastIndex]);
+  return result.concat(reverse(string.slice(0, lastIndex))).join('');
 };
 
 // 10. Write a function that determines if a string is a palindrome.
